756c1e7f4bf89ad7d8e31dd9568c6ddbdc7a5ab7
rzwilll <44703499+rzwilll@users.noreply.github.com>
2022-12-15 14:04:45 +0800

    ---

--- resources/views/modal/gen_report_content.blade.php
+++ resources/views/modal/gen_report_content.blade.php
@@ -1,16 +1,28 @@
- @foreach($data as $val)
- {{ $val->acad_yr }}
- @endforeach
- 
- <?php if(count($data) > 0){?>
-        <p>display generate button</p>
-<?php } else { ?>
-        <p>hide generate button</p>
-<?php }?>
 
-<button type="button" class="btn btn-secondary" onclick="modal_closed();">Close</button>
+
+    <label for="">Select</label>
+    <select name="" id="">
+        <option value="">Academic Year</option>
+        @foreach($data as $val)
+        <option value="{{$val->id}}">{{$val->acad_yr}}</option>
+        @endforeach
+ 
+    </select>
+
+    <br>
+    
+
+ 
+
 <?php
     if(count($data) > 0){?>
-        <button type="button" class="btn btn-primary">Generate</button>
+        <button type="button" class="btn btn-primary">Generate Report</button>
     <?php }
-?>
+    else { ?>
+        <p><small style = "color:red"> You already submitted required reports</small></p>
+
+   <?php }?>
+
+   <button type="button" class="btn btn-secondary" onclick="modal_closed();">Close</button>
+
+
